418dd7a61d7b053ada2e1ee905428398d308dfa7
Piyush kumar <90105030+whitehawk0910@users.noreply.github.com>
2024-02-22 21:45:55 +0530

    Add files via upload

---
+++ React-main/React-main/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Board from './board';
+//import Users from './Users.js'
+//import {useState} from 'react'
+function App() {
+
+return(
+<div className="App">
+  <Board />
+</div>
+);
+
+  //const[data,setData]=useState("piyush")
+  //function updateData(){
+    //setData("pk")
+  //}
+  //console.warn("____")
+  //return (
+    // <div className="App">
+      //  <h1>{data}</h1>
+      //  <button onClick={updateData}>UpdateData </button>
+     // </div>`
+ 
+};
+
+export default App;
